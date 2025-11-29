bdecae299c8e49b945de1b9951c4fea2c796c878
RuslanTsitser <cicer_ruslan@mail.ru>
2025-11-29 23:20:19 +0300

    feat(dependencies): update pubspec and lock for new plugins
    - Added `flutter_webrtc` and `permission_handler` dependencies to `pubspec.yaml`.
    - Updated `pubspec.lock` with new versions for `flutter_webrtc`, `permission_handler`, and related transitive dependencies.
    - Registered `flutter_webrtc` and `permission_handler_windows` plugins in platform-specific files for Linux and Windows.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_webrtc
 import path_provider_foundation
 import share_plus
 import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,8 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  flutter_webrtc
+  permission_handler_windows
   share_plus
   sqlite3_flutter_libs
   url_launcher_windows
