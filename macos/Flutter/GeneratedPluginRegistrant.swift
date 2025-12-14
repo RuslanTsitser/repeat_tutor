63e3d02cfe11e57116884bea9a925c7bc7aa5277
RuslanTsitser <cicer_ruslan@mail.ru>
2025-12-14 23:41:00 +0300

    feat(dependencies): add speech_to_text package and update related configurations
    - Added speech_to_text package version 7.3.0 to pubspec.yaml for voice recognition functionality.
    - Updated pubspec.lock to include new dependencies: pedantic, speech_to_text, speech_to_text_platform_interface, and speech_to_text_windows.
    - Modified iOS and macOS configurations to register the speech_to_text plugin.
    - Updated Info.plist to include NSSpeechRecognitionUsageDescription for microphone access permissions.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,6 +13,7 @@ import path_provider_foundation
 import record_macos
 import share_plus
 import shared_preferences_foundation
+import speech_to_text
 import sqlite3_flutter_libs
 import wakelock_plus
 
@@ -25,6 +26,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
