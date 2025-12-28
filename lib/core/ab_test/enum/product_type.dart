95660d031ee922a65eb0a107350c46cbae869a1a
RuslanTsitser <cicer_ruslan@mail.ru>
2025-12-28 17:32:42 +0300

    feat(ab_test): extend ProductType and RemoteConfig with new product fields
    - Added product1 and product2 to the ProductType enum for enhanced product differentiation.
    - Updated RemoteConfig to include new fields for product1, parentProduct1, product2, and parentProduct2, allowing for better configuration management.
    - Enhanced JSON serialization and copyWith methods to accommodate the new product fields, improving data handling and flexibility.

--- lib/core/ab_test/enum/product_type.dart
+++ lib/core/ab_test/enum/product_type.dart
@@ -3,13 +3,17 @@ import 'package:ab_test_service/ab_test_service.dart';
 import '../model/remote_config/remote_config.dart';
 
 enum ProductType implements BaseProductType {
-  main;
+  main,
+  product1,
+  product2;
 
   @override
   String? parentProductName(BaseRemoteConfig config) {
     if (config is RemoteConfig) {
       final productName = switch (this) {
         ProductType.main => config.parentMainProduct,
+        ProductType.product1 => config.parentProduct1,
+        ProductType.product2 => config.parentProduct2,
       };
       return productName;
     }
@@ -21,6 +25,8 @@ enum ProductType implements BaseProductType {
     if (config is RemoteConfig) {
       final productName = switch (this) {
         ProductType.main => config.mainProduct,
+        ProductType.product1 => config.product1,
+        ProductType.product2 => config.product2,
       };
       return productName;
     }
--- lib/core/ab_test/model/remote_config/remote_config.dart
+++ lib/core/ab_test/model/remote_config/remote_config.dart
@@ -5,6 +5,10 @@ class RemoteConfig extends BaseRemoteConfig {
   const RemoteConfig({
     this.mainProduct,
     this.parentMainProduct,
+    this.product1,
+    this.parentProduct1,
+    this.product2,
+    this.parentProduct2,
     super.onboarding,
     super.paywall,
     super.onboardingPaywall,
@@ -17,11 +21,27 @@ class RemoteConfig extends BaseRemoteConfig {
   /// String, продукт, от которого считаем скидку для [mainProduct]
   final String? parentMainProduct;
 
+  /// String, название продукта product1
+  final String? product1;
+
+  /// String, продукт, от которого считаем скидку для [product1]
+  final String? parentProduct1;
+
+  /// String, название продукта product2
+  final String? product2;
+
+  /// String, продукт, от которого считаем скидку для [product2]
+  final String? parentProduct2;
+
   @override
   Map<String, dynamic> toJson() {
     return <String, dynamic>{
       'mainProduct': mainProduct,
       'parentMainProduct': parentMainProduct,
+      'product1': product1,
+      'parentProduct1': parentProduct1,
+      'product2': product2,
+      'parentProduct2': parentProduct2,
       'onboarding': onboarding,
       'paywall': paywall,
       'onboardingPaywall': onboardingPaywall,
@@ -47,12 +67,24 @@ class RemoteConfig extends BaseRemoteConfig {
       parentMainProduct: map['parentMainProduct'] != null
           ? map['parentMainProduct'] as String
           : null,
+      product1: map['product1'] != null ? map['product1'] as String : null,
+      parentProduct1: map['parentProduct1'] != null
+          ? map['parentProduct1'] as String
+          : null,
+      product2: map['product2'] != null ? map['product2'] as String : null,
+      parentProduct2: map['parentProduct2'] != null
+          ? map['parentProduct2'] as String
+          : null,
     );
   }
 
   RemoteConfig copyWith({
     String? mainProduct,
     String? parentMainProduct,
+    String? product1,
+    String? parentProduct1,
+    String? product2,
+    String? parentProduct2,
     String? onboarding,
     String? paywall,
     String? onboardingPaywall,
@@ -61,6 +93,10 @@ class RemoteConfig extends BaseRemoteConfig {
     return RemoteConfig(
       mainProduct: mainProduct ?? this.mainProduct,
       parentMainProduct: parentMainProduct ?? this.parentMainProduct,
+      product1: product1 ?? this.product1,
+      parentProduct1: parentProduct1 ?? this.parentProduct1,
+      product2: product2 ?? this.product2,
+      parentProduct2: parentProduct2 ?? this.parentProduct2,
       onboarding: onboarding ?? this.onboarding,
       paywall: paywall ?? this.paywall,
       onboardingPaywall: onboardingPaywall ?? this.onboardingPaywall,
