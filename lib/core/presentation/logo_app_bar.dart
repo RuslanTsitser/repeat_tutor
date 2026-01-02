c22be563d8e9a8ce38db0736a0a923ac54267729
RuslanTsitser <cicer_ruslan@mail.ru>
2026-01-02 14:12:03 +0300

    feat(logo_app_bar): refactor LogoAppBar to use ConsumerWidget and add back button functionality
    - Changed LogoAppBar from StatelessWidget to ConsumerWidget to utilize Riverpod for state management.
    - Added new properties: showBackButton and withPadding for enhanced customization.
    - Implemented back button functionality using CupertinoButton for improved navigation.
    - Updated padding logic to conditionally apply based on withPadding property.

--- lib/core/presentation/logo_app_bar.dart
+++ lib/core/presentation/logo_app_bar.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../gen/assets.gen.dart';
+import '../../infrastructure/core.dart';
 import '../theme/app_colors.dart';
 import '../theme/app_text_style.dart';
 import 'pro_button.dart';
 
-class LogoAppBar extends StatelessWidget {
-  const LogoAppBar({super.key, this.showProButton = true});
+class LogoAppBar extends ConsumerWidget {
+  const LogoAppBar({
+    super.key,
+    this.showProButton = true,
+    this.showBackButton = false,
+    this.withPadding = true,
+  });
   final bool showProButton;
+  final bool showBackButton;
+  final bool withPadding;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.read(routerProvider);
     return Container(
       height: 64,
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      padding: withPadding
+          ? const EdgeInsets.symmetric(horizontal: 16, vertical: 8)
+          : EdgeInsets.zero,
       decoration: const BoxDecoration(
         borderRadius: BorderRadius.only(
           bottomLeft: Radius.circular(16),
           bottomRight: Radius.circular(16),
         ),
+        color: AppColors.surface,
         border: Border(
           bottom: BorderSide(
             color: AppColors.divider,
@@ -28,6 +41,12 @@ class LogoAppBar extends StatelessWidget {
       ),
       child: Row(
         children: [
+          if (showBackButton)
+            CupertinoButton(
+              padding: EdgeInsets.zero,
+              onPressed: () => router.pop(),
+              child: const Icon(CupertinoIcons.chevron_left),
+            ),
           Assets.appIcon.image(width: 48, height: 48),
           const SizedBox(width: 8),
           const Expanded(
